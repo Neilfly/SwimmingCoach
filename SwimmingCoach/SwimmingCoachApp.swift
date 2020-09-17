1687cc8d49b97fb8f47c388435a67677aa1c86df
Neil <643241432@qq.com>
2020-09-17 14:28:02 +0800

    Initial Commit

---
+++ SwimmingCoach/SwimmingCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwimmingCoachApp.swift
+//  SwimmingCoach
+//
+//  Created by 张雨飞 on 17/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SwimmingCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
