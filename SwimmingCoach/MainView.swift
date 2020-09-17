d87b7311d09208e149a73c08c1de27798fe6ddf5
Neil <643241432@qq.com>
2020-09-17 21:25:31 +0800

    优化了代码

--- SwimmingCoach/MainView.swift
+++ SwimmingCoach/MainView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MainView: View {
     
     @Binding var showSettings: Bool
+    @State var ModeCards = ModeCardArray
     
     var body: some View {
         VStack {
@@ -22,103 +23,17 @@ struct MainView: View {
                 HeadView(showSettings: $showSettings)
             }
             .padding(.horizontal)
+            .padding(.leading, 14)
             .padding(.top, 30)
             
             //三种不同模式的选择卡片
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 30.0) {
-                    //业余模式
-                    VStack {
-                        HStack(alignment: .top) {
-                            Text("业余游泳\n模式")
-                                .font(.system(size: 24, weight: .bold))
-                                .frame(width: 160, alignment: .leading)
-                                .foregroundColor(.white)
-                            
-                            Spacer()
-                            
-                            Text("易")
-                                .font(.system(size: 18, weight: .bold))
-                                .padding(6)
-                                .foregroundColor(Color(#colorLiteral(red: 0.368627451, green: 0.8, blue: 0.9725490196, alpha: 1)))
-                                .background(Color(#colorLiteral(red: 0.09803921569, green: 0.1215686275, blue: 0.2901960784, alpha: 1)))
-                                .clipShape(Circle())
-                        }
-                        
-                        Text("采用业余数据")
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        
-                        LottieView(filename: "businessStrategy")
-                            .frame(width: 210)
+
+                    ForEach(ModeCards.indices) { index in
+                        ModeCardView(ModeCards: $ModeCards[index])
                     }
-                    .padding(.top, 20)
-                    .padding(.horizontal, 20)
-                    .frame(width: 275, height: 275)
-                    .background(Color("c1"))
-                    .cornerRadius(30)
-                    .shadow(color: Color("c1").opacity(0.3), radius: 20, x: 0, y: 20)
                     
-                    //专业模式
-                    VStack {
-                        HStack(alignment: .top) {
-                            Text("专业游泳\n模式")
-                                .font(.system(size: 24, weight: .bold))
-                                .frame(width: 160, alignment: .leading)
-                                .foregroundColor(.white)
-                            
-                            Spacer()
-                            
-                            Text("难")
-                                .font(.system(size: 18, weight: .bold))
-                                .padding(6)
-                                .foregroundColor(Color(#colorLiteral(red: 0.368627451, green: 0.8, blue: 0.9725490196, alpha: 1)))
-                                .background(Color(#colorLiteral(red: 0.09803921569, green: 0.1215686275, blue: 0.2901960784, alpha: 1)))
-                                .clipShape(Circle())
-                        }
-                        
-                        Text("采用业余数据")
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        
-                        LottieView(filename: "businessTeam")
-                            .frame(width: 210)
-                    }
-                    .padding(.top, 20)
-                    .padding(.horizontal, 20)
-                    .frame(width: 275, height: 275)
-                    .background(Color("c3"))
-                    .cornerRadius(30)
-                    .shadow(color: Color("c3").opacity(0.3), radius: 20, x: 0, y: 20)
-                    
-                    //自由模式
-                    VStack {
-                        HStack(alignment: .top) {
-                            Text("自由游泳\n模式")
-                                .font(.system(size: 24, weight: .bold))
-                                .frame(width: 160, alignment: .leading)
-                                .foregroundColor(.white)
-                            
-                            Spacer()
-                            
-                            Text("自")
-                                .font(.system(size: 18, weight: .bold))
-                                .padding(6)
-                                .foregroundColor(Color(#colorLiteral(red: 0.368627451, green: 0.8, blue: 0.9725490196, alpha: 1)))
-                                .background(Color(#colorLiteral(red: 0.09803921569, green: 0.1215686275, blue: 0.2901960784, alpha: 1)))
-                                .clipShape(Circle())
-                        }
-                        
-                        Text("采用业余数据")
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        
-                        LottieView(filename: "socialMedia")
-                            .frame(width: 210)
-                    }
-                    .padding(.top, 20)
-                    .padding(.horizontal, 20)
-                    .frame(width: 275, height: 275)
-                    .background(Color("c2"))
-                    .cornerRadius(30)
-                    .shadow(color: Color("c2").opacity(0.3), radius: 20, x: 0, y: 20)
                 }
                 .padding(30)
                 .padding(.bottom, 30)
@@ -134,3 +49,55 @@ struct MainView_Previews: PreviewProvider {
         MainView(showSettings: .constant(false))
     }
 }
+
+struct ModeCardView: View {
+    
+    @Binding var ModeCards: ModeCard
+    
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text(ModeCards.title)
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                Text(ModeCards.icon)
+                    .font(.system(size: 18, weight: .bold))
+                    .padding(6)
+                    .foregroundColor(Color(#colorLiteral(red: 0.368627451, green: 0.8, blue: 0.9725490196, alpha: 1)))
+                    .background(Color(#colorLiteral(red: 0.09803921569, green: 0.1215686275, blue: 0.2901960784, alpha: 1)))
+                    .clipShape(Circle())
+            }
+            
+            Text(ModeCards.subtitle)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            LottieView(filename: ModeCards.lottie)
+                .frame(width: 210)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(Color(ModeCards.color))
+        .cornerRadius(30)
+        .shadow(color: Color(ModeCards.color).opacity(0.3), radius: 20, x: 0, y: 20)
+    }
+}
+
+struct ModeCard: Identifiable {
+    var id = UUID()
+    var icon: String
+    var title: String
+    var subtitle: String
+    var color: String
+    var lottie: String
+}
+
+let ModeCardArray = [
+    ModeCard(icon: "易", title: "业余游泳\n模式", subtitle: "采用业余数据", color: "c1", lottie: "businessStrategy"),
+    ModeCard(icon: "难", title: "专业游泳\n模式", subtitle: "采用专业数据", color: "c3", lottie: "businessTeam"),
+    ModeCard(icon: "自", title: "自由游泳\n模式", subtitle: "不进行数据对比", color: "c2", lottie: "socialMedia")
+]
