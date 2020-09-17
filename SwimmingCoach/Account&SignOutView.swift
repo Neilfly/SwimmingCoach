41c7704fe0c7455470aad7737f0fc3fae934ab35
Neil <643241432@qq.com>
2020-09-17 18:57:39 +0800

    setting界面的视图布局完成

--- SwimmingCoach/Account&SignOutView.swift
+++ SwimmingCoach/Account&SignOutView.swift
@@ -14,15 +14,39 @@ struct Account_SignOutView: View {
             Spacer()
             
             VStack(spacing: 30.0) {
+                Text("Neil - 游泳萌新")
+                    .font(.caption)
+                
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color.black.opacity(0.08))
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(12)
+                
                 SettingRow(title: "修改信息", icon: "gearshape")
                 SettingRow(title: "退出登陆", icon: "person.crop.circle")
             }
             .frame(maxWidth: .infinity)
             .frame(height: 300)
-            .background(Color.white)
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color.white, Color(#colorLiteral(red: 0.8705882353, green: 0.8941176471, blue: 0.9450980392, alpha: 1))]), startPoint: .top, endPoint: .bottom)
+            )
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(radius: 30)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .padding(.horizontal, 30)
+            .overlay(
+                Image("head")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .offset(y: -150)
+            )
         }
         .padding(.bottom, 30)
     }
@@ -45,6 +69,7 @@ struct SettingRow: View {
                 .font(.system(size: 20, weight: .light))
                 .imageScale(.large)
                 .frame(width: 32, height: 32)
+                .foregroundColor(Color(#colorLiteral(red: 0.715854045, green: 0.7333333333, blue: 0.831372549, alpha: 1)))
             
             Text(title)
                 .font(.system(size: 20, weight: .bold))
